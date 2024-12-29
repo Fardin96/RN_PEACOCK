05e2ffca609f82703e3a731ad4f1bede186e96f9
Fardin96 <fardinshuvro96@gmail.com>
2024-12-29 17:58:32 +0600

    like, comment, save buttons

--- screens/VideoPlayerScreen.tsx
+++ screens/VideoPlayerScreen.tsx
@@ -1,13 +1,18 @@
-import {StyleSheet, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
 import YoutubePlayer from 'react-native-youtube-iframe';
 import {useRoute} from '@react-navigation/native';
 import {SCREEN_WIDTH} from '../assets/dimensions/dimensions';
 import {VideoPlayerScreenRouteProp} from '../types/navigation';
+import {colors} from '../assets/colors/colors';
+import Icon from 'react-native-vector-icons/Entypo';
 
 function VideoPlayerScreen(): React.JSX.Element {
   const route = useRoute<VideoPlayerScreenRouteProp>();
   const {videoId} = route.params;
+
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [isSaved, setIsSaved] = useState<boolean>(false);
 
   return (
     <View style={styles.root}>
@@ -16,7 +21,43 @@ function VideoPlayerScreen(): React.JSX.Element {
         width={SCREEN_WIDTH}
         play={true}
         videoId={videoId}
+        webViewStyle={{borderWidth: 1, borderColor: 'green'}}
       />
+
+      <View
+        style={{
+          width: SCREEN_WIDTH,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          marginTop: -50,
+
+          // borderWidth: 1,
+          // borderColor: 'red',
+        }}>
+        {isLiked ? (
+          <TouchableOpacity onPress={() => setIsLiked(prev => !prev)}>
+            <Icon name="heart" size={30} color="red" />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={() => setIsLiked(prev => !prev)}>
+            <Icon name="heart-outlined" size={30} color="white" />
+          </TouchableOpacity>
+        )}
+
+        <TouchableOpacity onPress={() => {}}>
+          <Icon name="chat" size={30} color="white" />
+        </TouchableOpacity>
+
+        {isSaved ? (
+          <TouchableOpacity onPress={() => setIsSaved(prev => !prev)}>
+            <Icon name="save" size={30} color="red" />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={() => setIsSaved(prev => !prev)}>
+            <Icon name="save" size={30} color="white" />
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 }
@@ -27,7 +68,7 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: 'dimgray',
+    backgroundColor: colors.BG_PRIMARY,
 
     // borderWidth: 1,
     // borderColor: 'blue',
